adc7699c3648a9d5e8cc984eb566cc55ec968ad6
Athiriyya <athiriyya@gmail.com>
2023-06-14 16:14:58 -0500

    Quick-and-dirty commenting out to remove Japanese phonemization which relies on the MacOS-incompatible Mecab package, and ESpeak, which is also Linux & Windows-only.

--- TTS/tts/utils/text/phonemizers/__init__.py
+++ TTS/tts/utils/text/phonemizers/__init__.py
@@ -1,15 +1,16 @@
 from TTS.tts.utils.text.phonemizers.bangla_phonemizer import BN_Phonemizer
 from TTS.tts.utils.text.phonemizers.base import BasePhonemizer
-from TTS.tts.utils.text.phonemizers.espeak_wrapper import ESpeak
+# from TTS.tts.utils.text.phonemizers.espeak_wrapper import ESpeak
 from TTS.tts.utils.text.phonemizers.gruut_wrapper import Gruut
-from TTS.tts.utils.text.phonemizers.ja_jp_phonemizer import JA_JP_Phonemizer
+# from TTS.tts.utils.text.phonemizers.ja_jp_phonemizer import JA_JP_Phonemizer
 from TTS.tts.utils.text.phonemizers.ko_kr_phonemizer import KO_KR_Phonemizer
 from TTS.tts.utils.text.phonemizers.zh_cn_phonemizer import ZH_CN_Phonemizer
 
-PHONEMIZERS = {b.name(): b for b in (ESpeak, Gruut, JA_JP_Phonemizer)}
+# PHONEMIZERS = {b.name(): b for b in (ESpeak, Gruut, JA_JP_Phonemizer)}
+PHONEMIZERS = {b.name(): b for b in (Gruut,)}
 
 
-ESPEAK_LANGS = list(ESpeak.supported_languages().keys())
+# ESPEAK_LANGS = list(ESpeak.supported_languages().keys())
 GRUUT_LANGS = list(Gruut.supported_languages())
 
 
@@ -19,14 +20,14 @@ _ = [Gruut.name()] * len(GRUUT_LANGS)
 DEF_LANG_TO_PHONEMIZER = dict(list(zip(GRUUT_LANGS, _)))
 
 
-# Add ESpeak languages and override any existing ones
-_ = [ESpeak.name()] * len(ESPEAK_LANGS)
-_new_dict = dict(list(zip(list(ESPEAK_LANGS), _)))
-DEF_LANG_TO_PHONEMIZER.update(_new_dict)
+# # Add ESpeak languages and override any existing ones
+# _ = [ESpeak.name()] * len(ESPEAK_LANGS)
+# _new_dict = dict(list(zip(list(ESPEAK_LANGS), _)))
+# DEF_LANG_TO_PHONEMIZER.update(_new_dict)
 
 # Force default for some languages
 DEF_LANG_TO_PHONEMIZER["en"] = DEF_LANG_TO_PHONEMIZER["en-us"]
-DEF_LANG_TO_PHONEMIZER["ja-jp"] = JA_JP_Phonemizer.name()
+# DEF_LANG_TO_PHONEMIZER["ja-jp"] = JA_JP_Phonemizer.name()
 DEF_LANG_TO_PHONEMIZER["zh-cn"] = ZH_CN_Phonemizer.name()
 DEF_LANG_TO_PHONEMIZER["ko-kr"] = KO_KR_Phonemizer.name()
 DEF_LANG_TO_PHONEMIZER["bn"] = BN_Phonemizer.name()
@@ -42,14 +43,14 @@ def get_phonemizer_by_name(name: str, **kwargs) -> BasePhonemizer:
         kwargs (dict):
             Extra keyword arguments that should be passed to the phonemizer.
     """
-    if name == "espeak":
-        return ESpeak(**kwargs)
+    # if name == "espeak":
+    #     return ESpeak(**kwargs)
     if name == "gruut":
         return Gruut(**kwargs)
     if name == "zh_cn_phonemizer":
         return ZH_CN_Phonemizer(**kwargs)
-    if name == "ja_jp_phonemizer":
-        return JA_JP_Phonemizer(**kwargs)
+    # if name == "ja_jp_phonemizer":
+    #     return JA_JP_Phonemizer(**kwargs)
     if name == "ko_kr_phonemizer":
         return KO_KR_Phonemizer(**kwargs)
     if name == "bn_phonemizer":
